12a3ea0c0818446ac1c09d19f1f4e7f740d7a801
elvin <elvinmonteiro77@gmail.com>
2023-04-12 18:45:32 -0400

    update function to ruturn reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split('');
+    let lettersReversed = letters.reverse();
+    return lettersReversed.join('');
 };
 
 // Do not edit below this line
