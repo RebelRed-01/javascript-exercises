2d4e647622392c5fb3ae1c8cfd263a4ad18bb30f
elvin <elvinmonteiro77@gmail.com>
2023-04-27 17:37:00 -0400

    update function to convert temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let celsius = (tempInF - 32) * (5/9);
+  
+  if(celsius % 1 === 0) {
+    return celsius;
+  } else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let fahrenheit = (tempInC * 9/5) + 32;
+
+  if(fahrenheit % 1 === 0) {
+    return fahrenheit;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  
+  
 };
 
 // Do not edit below this line
