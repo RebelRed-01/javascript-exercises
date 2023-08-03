35cd53aa3e8c8a5d7b0b4b976297b20581e990b4
elvin <elvinmonteiro77@gmail.com>
2023-08-03 13:36:19 -0400

    update functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let result = 0;
+	for (let num of numArr) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let result = 1;
+	for (let num of numArr) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (let i = num; i > 1; i--) {
+    result *= num;
+    num --;
+  }
+  return result;
 };
 
 // Do not edit below this line
